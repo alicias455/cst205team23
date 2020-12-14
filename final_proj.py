aff3897a5453ce19433d10ec93fc9181cc3ce8a9
Alicia Sandoval <asandovalgarcia@csumb.edu>
2020-12-13 18:05:21 -0800

    working on displaying multiple images with Qt

--- final_proj.py
+++ final_proj.py
@@ -4,14 +4,9 @@ from PIL import Image
 import sys
 from PySide2.QtWidgets import (QApplication, QWidget, QLabel, QDialog, QGroupBox, QHBoxLayout, QVBoxLayout, QPushButton, QLineEdit,QComboBox)
 from PySide2.QtCore import Slot, Qt 
-
-
-
-
+from PySide2.QtGui import QPixmap
 
 #___________________list sorting___________________________
-
-#string1 ='scarlett johansson'.lower()
 
 new_dict = {}
 
@@ -46,19 +41,29 @@ class MyWindow(QWidget):
     self.label1 = QLabel('Enter Celebrity Full Name: ')
     self.line_edit = QLineEdit(self)
     preprocess(image_info)
+
+    self.label = QLabel()
+
     self.btn = QPushButton("Search")
     self.btn.clicked.connect(self.on_click)
+
 
     hbox1 = QHBoxLayout()
     hbox1.addWidget(self.label1)
     hbox1.addWidget(self.line_edit)
     hbox1.addWidget(self.btn)
 
+    self.vbox1 = QVBoxLayout()
+    #vbox1.addWidget(self.label)
+
     gbox1 = QGroupBox()
     gbox1.setLayout(hbox1)
+    self.gbox2 = QGroupBox()
+    #gbox2.setLayout(self.vbox1)
 
     mbox = QVBoxLayout()
     mbox.addWidget(gbox1)
+    mbox.addWidget(self.gbox2)
 
     self.setLayout(mbox)
     self.setWindowTitle("CST 205 Final Project")
@@ -84,9 +89,13 @@ class MyWindow(QWidget):
 
         size = len(max_list)
         for i in range(size):
-                print(i)
-                im = Image.open(f'images/{max_list[i][1]}.jpg')
-                im.show()
+                #print(i)
+                pixmap = QPixmap(f'images/{max_list[i][1]}.jpg')
+                self.label.setPixmap(pixmap)
+                self.vbox1.addWidget(self.label)
+                self.gbox2.setLayout(self.vbox1)
+                #im = Image.open(f'images/{max_list[i][1]}.jpg')
+                #im.show()
                 i += 1
 
 app = QApplication([])
